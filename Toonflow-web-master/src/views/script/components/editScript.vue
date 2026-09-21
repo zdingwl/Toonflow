93283954ea01e1fa72f0ad0f716b6b8dccbda543
zdingwl <139205911+zdingwl@users.noreply.github.com>
2026-09-21 13:33:50 +0800

    fix(script): allow editing imported episodes longer than 5000 characters

--- Toonflow-web-master/src/views/script/components/editScript.vue
+++ Toonflow-web-master/src/views/script/components/editScript.vue
@@ -14,7 +14,7 @@
               v-model="props.item.content"
               :placeholder="$t('workbench.script.edit.scriptContentPh')"
               :autosize="{ minRows: 20, maxRows: 20 }" />
-            <div class="scriptLen">{{ props.item.content.length }}/{{ otherSetting.scriptEpisodeLength }}</div>
+            <div class="scriptLen">{{ props.item.content.length }} {{ $t("workbench.novel.import.chars") }}</div>
           </div>
         </t-form-item>
         <t-form-item :label="$t('workbench.script.edit.relatedAssets')" name="assets">
@@ -39,7 +39,6 @@
         <t-button
           theme="primary"
           style="margin-left: 10px"
-          :disabled="props.item.content.length > otherSetting.scriptEpisodeLength"
           @click="onConfirm">
           保存
         </t-button>
@@ -51,8 +50,6 @@
 <script setup lang="ts">
 import axios from "@/utils/axios";
 import openAssetsSelector from "@/utils/assetsCheck";
-import settingStore from "@/stores/setting";
-const { otherSetting } = storeToRefs(settingStore());
 interface ScriptAsset {
   id: number;
   name: string;
@@ -136,7 +133,6 @@ async function onConfirm() {
       .assets-list {
         display: flex;
         flex-wrap: wrap;
-        gap: 8px;
       }
       .assets-empty {
         font-size: 13px;
