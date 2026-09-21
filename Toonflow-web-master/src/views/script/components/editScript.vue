7b52bacbf5aacc4884e404a72042b1f26509ef3f
zdingwl <139205911+zdingwl@users.noreply.github.com>
2026-09-21 13:45:34 +0800

    fix(script): apply configured per-episode length on edit screen

--- Toonflow-web-master/src/views/script/components/editScript.vue
+++ Toonflow-web-master/src/views/script/components/editScript.vue
@@ -14,7 +14,7 @@
               v-model="props.item.content"
               :placeholder="$t('workbench.script.edit.scriptContentPh')"
               :autosize="{ minRows: 20, maxRows: 20 }" />
-            <div class="scriptLen">{{ props.item.content.length }} {{ $t("workbench.novel.import.chars") }}</div>
+            <div class="scriptLen">{{ props.item.content.length }}/{{ otherSetting.scriptEpisodeLength }}</div>
           </div>
         </t-form-item>
         <t-form-item :label="$t('workbench.script.edit.relatedAssets')" name="assets">
@@ -39,6 +39,7 @@
         <t-button
           theme="primary"
           style="margin-left: 10px"
+          :disabled="props.item.content.length > otherSetting.scriptEpisodeLength"
           @click="onConfirm">
           保存
         </t-button>
@@ -50,6 +51,8 @@
 <script setup lang="ts">
 import axios from "@/utils/axios";
 import openAssetsSelector from "@/utils/assetsCheck";
+import settingStore from "@/stores/setting";
+const { otherSetting } = storeToRefs(settingStore());
 interface ScriptAsset {
   id: number;
   name: string;
@@ -99,6 +102,10 @@ function removeAsset(id: number) {
 const emit = defineEmits(["searchScripts"]);
 //确认
 async function onConfirm() {
+  if (props.item.content.length > otherSetting.value.scriptEpisodeLength) {
+    window.$message.warning(`单集剧本不能超过 ${otherSetting.value.scriptEpisodeLength} 字，请调整单集字数设置或缩短本集内容`);
+    return;
+  }
   try {
     await axios.post("/script/updateScript", {
       id: props.item.id,
